30afd2bfa0d045e2cb991484499610378d810ae0
Musa ÖZDEMİR <musa.ozdemir.tr@gmail.com>
2018-07-16 22:32:54 +0300

    d

--- js/app.js
+++ js/app.js
@@ -4,10 +4,7 @@ var app = new Vue({
     dizi:{}
   },
   created:function(){
-    this.$http.get('http://jsonplaceholder.typicode.com/posts/{/id}').then(response => {
-
-   // get body data
-   //this.someData = response.body;
+    this.$http.get('http://jsonplaceholder.typicode.com/posts').then(response => {
    console.log(response.body);
    this.dizi=response.body;
 
